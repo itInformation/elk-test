14a67bcab68b76fd75da4e47b346fefa695a8b3c
zhangminghui6106 <zhangminghui6106@navinfo.com>
2021-08-09 11:01:26 +0800

    elk 小试牛刀

---
+++ src/test/java/com/learn/elk/elktest/ElkTestApplicationTests.java
@@ -0,0 +1,20 @@
+package com.learn.elk.elktest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkTestApplicationTests {
+
+	//创建日志对象
+	Logger logger = LogManager.getLogger(this.getClass());
+
+	@Test
+	public void test1() {
+		logger.info("logback的日志信息过来了");
+		logger.error("logback的错误信息过来了");
+	}
+
+}
